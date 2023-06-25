d24892d0b29b3831c020150a5c41dfbfaad6067b
Asif Ali <62379306+Aasif9@users.noreply.github.com>
2023-06-26 00:31:59 +0530

    Delete Green-Stitch-Assignment/src/main/java/com/example/GreenStitchAssignment directory

--- Green-Stitch-Assignment/src/main/java/com/example/GreenStitchAssignment/JWT/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.GreenStitchAssignment.JWT;
-
-public interface SecurityConstants {
-
-    public static final String JWT_KEY ="asifalietos$25$memory~master99~";
-    public static final String JWT_HEADER ="Authorization";
-}
--- Green-Stitch-Assignment/src/main/java/com/example/GreenStitchAssignment/Services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.GreenStitchAssignment.Services;
-
-import Model.UserDetail;
-import jdk.jshell.spi.ExecutionControl;
-
-public interface UserService {
-    public UserDetail registerUser(UserDetail user) throws ExecutionControl.UserException;
-    public UserDetail loginUser()  throws ExecutionControl.UserException;
-}
